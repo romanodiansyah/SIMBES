4ba090990795d084a52341455ae25c35c9b2ea72
prasetyopamb <prasetyopambudi11@gmail.com>
2018-11-13 16:04:24 +0700

    tambah komponen login-page

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeDashboardComponent } from './home-dashboard/home-dashboard.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeDashboardComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule,
