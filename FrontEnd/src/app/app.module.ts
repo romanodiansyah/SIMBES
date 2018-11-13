7831463e1e2126d9336ea8570b7106f1eb651eef
prasetyopamb <prasetyopambudi11@gmail.com>
2018-11-13 16:05:31 +0700

    add login page

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -3,13 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeDashboardComponent } from './home-dashboard/home-dashboard.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeDashboardComponent,
     LoginPageComponent
   ],
   imports: [
