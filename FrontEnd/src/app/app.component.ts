4888991519cad556ec887d8e8921e5e15455da33
prasetyopamb <prasetyopambudi11@gmail.com>
2018-11-30 21:34:06 +0700

    edit login page

--- FrontEnd/src/app/app.component.ts
+++ FrontEnd/src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
+  myForm : FormGroup;
+  email: FormControl;
+  password: FormControl;
+  constructor(private router: Router) {}
+
+
   title = 'mppl';
+
+  loginPage = LoginPageComponent;
+
+  ngOnInit() {
+    this.email = new FormControl
+  }
+
+  openLogin(): void {
+    this.router.navigate(['app/login-Page'])
+  }
+
 }
--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { RegisterPageComponent } from './register-page/register-page.component';
+import { FormGroup } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,9 @@ import { RegisterPageComponent } from './register-page/register-page.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule,
+    FormGroup
   ],
   providers: [],
   bootstrap: [AppComponent]
