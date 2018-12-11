263423b7ad42ed668f7652e33ab91238cff48ea2
Mochamad Suryono <mochamad_suryono@apps.ipb.ac.id>
2018-12-12 01:21:38 +0700

    dashboard (nambah Beasiswa) | forms (tambah beasiswa)

---
+++ template-frontend/src/app/main/pages/beasiswa/tambah-beasiswa/tambah-beasiswa.controller.js
@@ -0,0 +1,159 @@
+(function ()
+{
+    'use strict';
+
+    angular
+        .module('app.pages.beasiswa.tambah-beasiswa')
+        .controller('TambahBeasiswaController', TambahBeasiswaController);
+
+    /** @ngInject */
+    function TambahBeasiswaController($mdDialog)
+    {
+        var vm = this;
+
+        // Data
+        vm.horizontalStepper = {
+            step1: {},
+            step2: {},
+            step3: {},
+            step4: {},
+            step5: {}
+        };
+
+        vm.verticalStepper = {
+            step1: {},
+            step2: {},
+            step3: {},
+            step4: {},
+            step5: {}
+        };
+
+        vm.basicForm = {};
+        vm.formWizard = {};
+        vm.semester = ('1 2 3 4 5 6 7 8 ' +
+        '9 10 11 12').split(' ').map(function (semester)
+        {
+            return {abbrev: semester};
+        });
+
+        // Methods
+        vm.sendForm = sendForm;
+        vm.showDataDialog = showDataDialog;
+        vm.submitHorizontalStepper = submitHorizontalStepper;
+        vm.submitVerticalStepper = submitVerticalStepper;
+
+        //////////
+
+        /**
+         * Submit horizontal stepper data
+         * @param event
+         */
+        function submitHorizontalStepper(event)
+        {
+            // Show the model data in a dialog
+            vm.showDataDialog(event, vm.horizontalStepper);
+
+            // Reset the form model
+            vm.horizontalStepper = {
+                step1: {},
+                step2: {},
+                step3: {},
+                step4: {},
+                step5: {}
+            };
+        }
+
+        /**
+         * Submit vertical stepper data
+         *
+         * @param event
+         */
+        function submitVerticalStepper(event)
+        {
+            // Show the model data in a dialog
+            vm.showDataDialog(event, vm.verticalStepper);
+
+            // Reset the form model
+            vm.verticalStepper = {
+                step1: {},
+                step2: {},
+                step3: {},
+                step4: {},
+                step5: {}
+            };
+        }
+
+        /**
+         * Submit stepper form
+         *
+         * @param ev
+         */
+        function showDataDialog(ev, data)
+        {
+            // You can do an API call here to send the form to your server
+
+            // Show the sent data.. you can delete this safely.
+            $mdDialog.show({
+                controller         : function ($scope, $mdDialog, formWizardData)
+                {
+                    $scope.formWizardData = formWizardData;
+                    $scope.closeDialog = function ()
+                    {
+                        $mdDialog.hide();
+                    };
+                },
+                template           : '<md-dialog>' +
+                '  <md-dialog-content><h1>You have sent the form with the following data</h1><div><pre>{{formWizardData | json}}</pre></div></md-dialog-content>' +
+                '  <md-dialog-actions>' +
+                '    <md-button ng-click="closeDialog()" class="md-primary">' +
+                '      Close' +
+                '    </md-button>' +
+                '  </md-dialog-actions>' +
+                '</md-dialog>',
+                parent             : angular.element('body'),
+                targetEvent        : ev,
+                locals             : {
+                    formWizardData: data
+                },
+                clickOutsideToClose: true
+            });
+        }
+
+        /**
+         * Send form
+         */
+        function sendForm(ev)
+        {
+            // You can do an API call here to send the form to your server
+
+            // Show the sent data.. you can delete this safely.
+            $mdDialog.show({
+                controller         : function ($scope, $mdDialog, formWizardData)
+                {
+                    $scope.formWizardData = formWizardData;
+                    $scope.closeDialog = function ()
+                    {
+                        $mdDialog.hide();
+                    };
+                },
+                template           : '<md-dialog>' +
+                '  <md-dialog-content><h1>You have sent the form with the following data</h1><div><pre>{{formWizardData | json}}</pre></div></md-dialog-content>' +
+                '  <md-dialog-actions>' +
+                '    <md-button ng-click="closeDialog()" class="md-primary">' +
+                '      Close' +
+                '    </md-button>' +
+                '  </md-dialog-actions>' +
+                '</md-dialog>',
+                parent             : angular.element('body'),
+                targetEvent        : ev,
+                locals             : {
+                    formWizardData: vm.formWizard
+                },
+                clickOutsideToClose: true
+            });
+
+            // Clear the form data
+            vm.formWizard = {};
+        }
+    }
+})();
--- template-frontend/src/app/main/pages/pages.module.js
+++ template-frontend/src/app/main/pages/pages.module.js
@@ -4,21 +4,23 @@
 
     angular
         .module('app.pages', [
-            'app.pages.auth.login',
-            'app.pages.auth.login-v2',
-            'app.pages.auth.register',
-            'app.pages.auth.register-v2',
-            'app.pages.auth.forgot-password',
-            'app.pages.auth.reset-password',
-            'app.pages.auth.lock',
+            // 'app.pages.auth.login',
+            // 'app.pages.auth.login-v2',
+            // 'app.pages.auth.register',
+            // 'app.pages.auth.register-v2',
+            // 'app.pages.auth.forgot-password',
+            // 'app.pages.auth.reset-password',
+            // 'app.pages.auth.lock',
             // 'app.pages.coming-soon',
             // 'app.pages.error-404',
             // 'app.pages.error-500',
-            'app.pages.invoice',
-            'app.pages.maintenance',
+            // 'app.pages.invoice',
+            // 'app.pages.maintenance',
             'app.pages.profile',
-            'app.pages.search',
-            'app.pages.timeline'
+            // 'app.pages.search',
+            // 'app.pages.timeline',
+			'app.pages.beasiswa',
+			'app.pages.beasiswa.tambah-beasiswa'
         ])
         .config(config);
 
