b9248f9d9e48eb2f440ba40c100f96e40a62d5e9
Mochamad Suryono <mochamad_suryono@apps.ipb.ac.id>
2018-12-14 18:17:54 +0700

    Login + Register

---
+++ template-frontend/src/app/main/pages/auth/auth.module.js
@@ -0,0 +1,36 @@
+(function ()
+{
+    'use strict';
+
+    angular
+        .module('app.pages.auth',
+            [
+                'app.pages.auth.register-v2',
+				'app.pages.auth.login-v2'
+            ]
+        )
+        .config(config);
+
+    /** @ngInject */
+    function config(msNavigationServiceProvider)
+    {
+        // Navigation
+		msNavigationServiceProvider.saveItem('pages.auth', {
+            title : 'User',
+            icon  : 'icon-account',
+			state : 'app.pages.auth',
+            weight: 1
+        });
+
+        msNavigationServiceProvider.saveItem('pages.auth.register-v2', {
+            title: 'Register Admin',
+            state: 'app.pages_auth_register-v2'
+        });
+		
+		msNavigationServiceProvider.saveItem('pages.auth.login-v2', {
+            title: 'Login Admin',
+            state: 'app.pages_auth_register-v2'
+        });
+    }
+
+})();
