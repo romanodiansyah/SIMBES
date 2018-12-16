cc3e29b825dab0de15ca77bdb2c4555f3280c97c
Sony Vaio <nuhsatria@gmail.com>
2018-12-16 19:36:27 +0700

    routing api + service pambudi

--- FrontEnd/src/app/Services/auth.service.ts
+++ FrontEnd/src/app/Services/auth.service.ts
@@ -13,8 +13,38 @@ const url = "http://127.0.0.1:8000/api/auth";
 export class AuthService {
 
   public data: any;
+  public token: any;
 
-  constructor(private http: HttpClient) { }
+  HAS_LOGGED_IN = 'hasLoggedIn';
+
+  constructor(private http: HttpClient, private storage: Storage) { }
+
+  hasLoggedIn(): Promise<boolean> {
+    return this.storage.get(this.HAS_LOGGED_IN).then((value) => { 
+      return value === true;
+    });
+  };
+
+  login(credentials) {
+    return new Promise((resolve, reject) => {
+        let headers = new HttpHeaders();
+        headers.append('Content-Type', 'application/json');
+        headers.append('Accept', 'application/json');
+
+        this.http.post(url+'/login', JSON.stringify(credentials), {headers: headers})
+          .subscribe(res => {
+              // localStorage.setItem('token', res.data.meta.token);
+              this.storage.set(this.HAS_LOGGED_IN, true);
+              resolve(res);
+            
+
+           }, (err) => {
+            reject(err);
+          });
+    });
+  }
+
+
   register(credentials) {
     return new Promise((resolve, reject) => {
       let headers = new HttpHeaders();
@@ -27,10 +57,7 @@ export class AuthService {
       this.http.post(url+'/admin/register', credentials, {headers: headers})
         .subscribe(res => {
           console.log("Masuk");
-          if(res == 201) {
-            console.log("Yeah berhasil");
-            resolve(res);
-          }
+          resolve(res);
           
         }, (err) => {
           reject(err);
