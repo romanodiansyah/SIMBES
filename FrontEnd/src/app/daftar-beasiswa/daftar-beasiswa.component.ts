6c536bc82fe6997a3bf41567a181e265a0727a16
Sony Vaio <nuhsatria@gmail.com>
2018-12-16 20:20:40 +0700

    daftar succses

--- FrontEnd/src/app/daftar-beasiswa/daftar-beasiswa.component.ts
+++ FrontEnd/src/app/daftar-beasiswa/daftar-beasiswa.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Router, ActivatedRoute } from '@angular/router';
+import { RequestOptions } from '@angular/http';
+
+const url = "http://localhost:8000/api/";
 
 @Component({
   selector: 'app-daftar-beasiswa',
@@ -7,8 +12,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DaftarBeasiswaComponent implements OnInit {
 
-  constructor() { }
+  token: any;
+  pendaftarData : {id_beasiswa?:any; id_user?:any; status?:number; status_aktif?:number; alamat_berkas?:any}={};
+
+  constructor(private http: HttpClient, private router:Router, private route:ActivatedRoute) {
+    
+  }
   
+  daftar(){
+    console.log("daftar");
+    const id_beasiswa = this.route.snapshot.params["id"];
+    const id_user = localStorage.getItem('id_user');
+    this.pendaftarData.id_beasiswa = id_beasiswa;
+    this.pendaftarData.id_user = id_user;
+    this.pendaftarData.status = 1;
+    this.pendaftarData.status_aktif = 1;
+    this.pendaftarData.alamat_berkas = 0; 
+
+
+    let token = localStorage.getItem("token");
+    let headers = new HttpHeaders({'Authorization':'Bearer '+token, 'Content-Type':'application/json'})
+    // let options = new RequestOptions({headers: headers});
+
+    this.http.post(url+"pendaftar/create", this.pendaftarData, {headers:headers}).subscribe((res:any) => {
+      console.log("sucsess", res)
+      this.router.navigate(['']);
+    }, err=>{
+      console.log('error nih', err)
+    });
+  }
+
   ngOnInit() {
   }
 
